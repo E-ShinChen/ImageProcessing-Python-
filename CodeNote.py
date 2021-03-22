7538461307fba78bf03706aac8e7b0c48081d010
E-ShinChen <49832363+E-ShinChen@users.noreply.github.com>
2021-03-22 17:26:51 +0800

    Update CodeNote.py

--- CodeNote.py
+++ CodeNote.py
@@ -6,4 +6,12 @@ os.makedirs("filepath")
 
 import shutil
 
-shutil.copyfile("oldfile","newfile")
+shutil.move("oldfile","newfile")
+shutil.copyfile("oldfile","newfile") # 只能是文件
+shutil.copy("oldfile","newfile")  # # oldfile只能是文件夹，newfile可以是文件，也可以是目錄
+
+
+# String process
+list = 'I love you'
+list.split(' ',1) 
+list.rsplit(' ')
